1fbad52edb68592ea753b9a4ab5f26091136d81a
Damasoumana1 <soumanadama93@gmail.com>
2024-06-22 18:30:42 +0000

    Projet de service de Batiment et de traveau public(html,css,js)

---
+++ js/bpws.js
@@ -0,0 +1,7 @@
+const menuHamburger = document.
+querySelector(".menu-hamburger");
+const ul = document.quertSelector
+("ul")
+menuHamburger.addEventListener
+('click',()=>{ul.classList.toggle
+('mobile-menu')});
---
+++ js/footer.js
@@ -0,0 +1,5 @@
+var year=new Date().getFullYear();
+var copy='Copyright,';
+var all='All Right ';
+
+document.getElementsByTagName('mentions')[0].innerHTML=copy + year + all;
